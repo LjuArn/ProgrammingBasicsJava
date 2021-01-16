0d945f65a04e92769525a14fcefa9a8bc38a0da2
LjuArn <76203414+LjuArn@users.noreply.github.com>
2021-01-16 09:10:39 +0200

    Add files via upload

---
+++ NumbersEndingin7.java
@@ -0,0 +1,13 @@
+package forCicleEX;
+
+public class NumbersEndingin7 {
+
+    public static void main(String[] args) {
+
+        for (int i =7; i<= 997; i= i + 10){
+
+            System.out.println(i);
+        }
+
+    }
+}
